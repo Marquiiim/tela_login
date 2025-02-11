82882674450d82491925048ff763dd3bff05c753
Marcos Gabriel <marcosindev@gmail.com>
2025-02-11 17:20:50 -0300

    Organização de código e arquivo

--- main.py
+++ main.py
@@ -1,48 +1,100 @@
-import datetime
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import ObjectProperty
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+from database import DataBase
 
-class DataBase:
-    def __init__(self, filename):
-        self.filename = filename
-        self.users = None
-        self.file = None
-        self.load()
-    
-    def load(self):
-        self.file = open(self.filename, "r")
-        self.users = {}
+class CreateAccountWindow(Screen):
+    namee = ObjectProperty(None)
+    email = ObjectProperty(None)
+    password = ObjectProperty(None)
 
-        for line in self.file:
-            email, password, name, created = line.strip().split(";")
-            self.users[email] = (password, name, created)
-        
-        self.file.close()
-    
-    def get_user(self, email):
-        if email in self.users:
-            return self.users[email]
+    def submit(self):
+        if self.namee.text != "" and self.email.text != "" and self.email.text.count("@") == 1 and self.email.text.count(".") > 0:
+            if self.password.text != "":
+                db.add_user(self.email.text, self.password.text, self.namee.text)
+                self.reset()
+                sm.current = "login"
+            else:
+                self.invalid_form()
         else:
-            return -1
-        
-    def add_user(self, email, password, name):
-        if email.strip() not in self.users:
-            self.users[email.strip()] = (password.strip(), name.strip(), DataBase.get_date())
-            self.save()
-            return 1
-        else:
-            print("Email já existe!")
-            return -1
-        
-    def validate(self, email, password):
-        if self.get_user(email) != -1:
-            return self.users[email][0] == password
-        else: 
-            return False
-        
-    def save(self):
-        with open(self.filename, "w") as f:
-            for user in self.users:
-                f.write(user + ";" + self.users[user][0] + ";" + self.users[user][1] + ";" + self.users[user][2] + "\n")
+            self.invalid_form()
 
-                @staticmethod
-                def get_date():
-                    return str(datetime.datetime.now()).split(" ")[0]
+    def login(self):
+        self.reset()
+        sm.current = "login"
+
+    def reset(self):
+        self.email.text = ""
+        self.password.text = ""
+        self.namee.text = ""
+
+    def invalid_form(self):
+        pop = Popup(title='Invalid Form',
+                    content=Label(text='Please fill in all inputs with valid information.'),
+                    size_hint=(None, None), size=(400, 400))
+        pop.open()
+
+class LoginWindow(Screen):
+    email = ObjectProperty(None)
+    password = ObjectProperty(None)
+
+    def loginBtn(self):
+        if db.validate(self.email.text, self.password.text):
+            MainWindow.current = self.email.text
+            self.reset()
+            sm.current = "main"
+        else:
+            self.invalid_login()
+
+    def createBtn(self):
+        self.reset()
+        sm.current = "create"
+
+    def reset(self):
+        self.email.text = ""
+        self.password.text = ""
+
+    def invalid_login(self):
+        pop = Popup(title='Invalid Login',
+                    content=Label(text='Invalid username or password.'),
+                    size_hint=(None, None), size=(400, 400))
+        pop.open()
+
+class MainWindow(Screen):
+    n = ObjectProperty(None)
+    created = ObjectProperty(None)
+    email = ObjectProperty(None)
+    current = ""
+
+    def logOut(self):
+        sm.current = "login"
+
+    def on_enter(self, *args):
+        password, name, created = db.get_user(self.current)
+        self.n.text = "Account Name: " + name
+        self.email.text = "Email: " + self.current
+        self.created.text = "Created On: " + created
+
+class WindowManager(ScreenManager):
+    pass
+
+kv = Builder.load_file("my.kv")
+sm = WindowManager()
+db = DataBase("users.txt")
+
+screens = [LoginWindow(name="login"), CreateAccountWindow(name="create"), MainWindow(name="main")]
+
+for screen in screens:
+    sm.add_widget(screen)
+
+sm.current = "login"
+
+class MyMainApp(App):
+    def build(self):
+        return sm
+
+if __name__ == "__main__":
+    MyMainApp().run()
